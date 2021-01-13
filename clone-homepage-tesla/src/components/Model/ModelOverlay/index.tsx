787f2b77224f6193b9b6cb42a0acff327a9e0086
HENRIQUE <henrique_holtz@hotmail.com>
2021-01-12 23:33:12 -0300

    Pointer Events Issue

--- clone-homepage-tesla/src/components/Model/ModelOverlay/index.tsx
+++ clone-homepage-tesla/src/components/Model/ModelOverlay/index.tsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container } from './styles';
 import useWrapperScroll from '../useWrapperScroll';
+import { CarModel } from '../ModelsContext';
+import { useCallback, useLayoutEffect } from 'react';
+import { useTransform } from 'framer-motion';
 
-const ModelOverlay: React.FC = ({ children }) => {
+interface Props {
+  model: CarModel
+}
+
+type SectionDimensions = Pick<HTMLDivElement, 'offsetTop' | 'offsetHeight'>
+
+const ModelOverlay: React.FC<Props> = ({ model, children }) => {
+
+  const getSectionDimensions = useCallback(() => {
+    return {
+      offsetTop: model.sectionRef.current?.offsetTop,
+      offsetHeight: model.sectionRef.current?.offsetHeight
+    } as SectionDimensions
+  }, [model.sectionRef])
+
+  const [dimensions, setDimensions] = useState<SectionDimensions>(getSectionDimensions())
+
+  useLayoutEffect(() => {
+    function onResize() {
+      window.requestAnimationFrame(() => setDimensions(getSectionDimensions()))
+    }
+    window.addEventListener("resize", onResize)
+
+    return () => window.removeEventListener('resize', onResize)
+  }, [getSectionDimensions])
+
   const { scrollY } = useWrapperScroll()
 
+  const sectionScrollProgress = useTransform(scrollY, y => (y - dimensions.offsetTop) / dimensions.offsetHeight)
+
+  const opacity = useTransform(
+    sectionScrollProgress,
+    [-0.42, -0.05, 0.05, 0.42],
+    [0, 1, 1, 0]
+  )
+
   return (
-    <Container>{children}</Container>
+    <Container style={{ opacity }}>{children}</Container>
   );
 };
 
