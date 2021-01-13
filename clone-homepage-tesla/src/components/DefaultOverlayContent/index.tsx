eeb25116551fa0be7c45d801972865870e6ead2c
HENRIQUE <henrique_holtz@hotmail.com>
2021-01-12 22:51:34 -0300

    Hook of scroll

--- clone-homepage-tesla/src/components/DefaultOverlayContent/index.tsx
+++ clone-homepage-tesla/src/components/DefaultOverlayContent/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container/*, Heading, Buttons*/ } from './styles';
+import { Container, Heading, Buttons } from './styles';
 
 interface Props {
   label: string
@@ -12,15 +12,15 @@ const DefaultOverlayContent: React.FC<Props> = ({
 }) => {
   return (
     <Container>
-      {/* <Heading> */}
-      <h1>{label}</h1>
-      <h2>{description}</h2>
-      {/* </Heading> */}
+      <Heading>
+        <h1>{label}</h1>
+        <h2>{description}</h2>
+      </Heading>
 
-      {/* <Buttons> */}
-      <button>Custom Order</button>
-      <button className="white">Existing Inventory</button>
-      {/* </Buttons> */}
+      <Buttons>
+        <button>Custom Order</button>
+        <button className="white">Existing Inventory</button>
+      </Buttons>
     </Container>
   );
 };
--- clone-homepage-tesla/src/components/Model/ModelOverlay/index.tsx
+++ clone-homepage-tesla/src/components/Model/ModelOverlay/index.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 import { Container } from './styles';
 
-const ModelOverlay: React.FC = () => {
+const ModelOverlay: React.FC = ({ children }) => {
   return (
-    <Container>
-      <h1>ModelOverlay</h1>
-    </Container>
+    <Container>{children}</Container>
   );
 };
 
