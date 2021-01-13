48cc829344b68578df7e921fffccf1b956354095
HENRIQUE <henrique_holtz@hotmail.com>
2021-01-12 23:10:01 -0300

    Overlay of animation

--- clone-homepage-tesla/src/components/Model/ModelOverlay/index.tsx
+++ clone-homepage-tesla/src/components/Model/ModelOverlay/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 import { Container } from './styles';
+import useWrapperScroll from '../useWrapperScroll';
 
 const ModelOverlay: React.FC = ({ children }) => {
+  const { scrollY } = useWrapperScroll()
+
   return (
     <Container>{children}</Container>
   );
